7bd7c710f9de6f2f1712031e5bbd071c20f19f58
Wen Xiao <wennstyle@gmail.com>
2022-05-08 20:40:02 -0400

    commit tree 9ad528e6431c436f2d3eb8a25422a40b0d09e32e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).filter{|sentence| !sentence.empty?}.length   
+  #filter method to find empty strings due to using mutliple punctuation back to back. The ! is used to find non empty values in the array
   end
 end
